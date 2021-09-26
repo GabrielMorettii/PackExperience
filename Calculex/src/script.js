0ff31456315d2431f1e623762e38ce9f465e80dc
GabrielMoretti <gabrielmorettisilva@gmail.com>
2021-09-26 15:19:20 -0300

    Calculex: Structure

---
+++ Calculex/src/script.js
@@ -0,0 +1,20 @@
+const openCalc = document.getElementById('openCalc');
+const container = document.getElementsByClassName('container')[0];
+const close = document.getElementById('close');
+const screen = document.getElementById('screen');
+const buttons = document.getElementsByClassName('button');
+const equal = document.getElementById('equal');
+
+let numbers = document.getElementById('numbers');
+let numberScreen = document.getElementById('numberScreen');
+
+let sum = 0; 
+
+openCalc.addEventListener('click', () => {
+  container.classList.add('active');
+  openCalc.classList.add('opened');
+  screen.classList.add('active');
+})
+
+
+
