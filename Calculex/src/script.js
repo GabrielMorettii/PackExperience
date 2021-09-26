eecef9ca92f23bb81b172d75971cff4d5fe675db
GabrielMoretti <gabrielmorettisilva@gmail.com>
2021-09-26 15:20:34 -0300

    Calculex: Script of Five Operations

--- Calculex/src/script.js
+++ Calculex/src/script.js
@@ -22,3 +22,67 @@ close.addEventListener('click', ()=>{
   screen.classList.remove('active');
 })
 
+let num1 = "", num2 = "", operation = "";
+
+for (const button of buttons) {
+  button.addEventListener('click', ()=>{
+    let content = button.innerText;
+
+    if(content === "AC") {
+      numberScreen.innerText = "";
+      numbers.innerText = "";
+      num1 = "";
+      num2 = "";
+      operation = "";
+      sum = "";
+      
+      return;
+    } 
+
+    if(content === "="){
+      num1 = Number(num1);
+      num2 = Number(num2);
+
+      numberScreen.innerText = "";
+
+      switch(operation){
+        case '+':
+          sum = num2 + num1;
+          break;
+        case '-':
+          sum = num2 - num1;
+          break;
+        case 'x':
+          sum = num2 * num1;
+          break;
+        case '/':
+          sum = num2 / num1;
+          break;
+        case '%':
+          sum = num2 % num1;
+          break;
+        case '^':
+          sum = num2 ** num1;
+          break;
+      }
+
+      numberScreen.innerText = String(sum);
+      numbers.innerText = "";
+      num1 = "";
+      num2 = "";
+      operation = "";
+    } else if (!Number(content)){
+      num2 += num1;
+      num1 = "";
+
+      operation += content;
+
+      numbers.innerText += content;
+    } else{
+      num1 += content;
+
+      numbers.innerText += content;
+    }
+
+  })
+}
