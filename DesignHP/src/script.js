1c72d5ff0e02898b5d7bd6b7593e1b42e3fe7a4e
GabrielMoretti <gabrielmorettisilva@gmail.com>
2021-09-25 23:01:26 -0300

    Chat

---
+++ DesignHP/src/script.js
@@ -0,0 +1,13 @@
+let contato = document.getElementById('contato');
+let chat = document.getElementsByClassName('chat')[0];
+let close = document.getElementById('close');
+
+
+contato.addEventListener('click', () => {
+  if(chat.classList.contains('active')){
+    chat.classList.remove('active')
+  }else{
+    chat.classList.add('active')
+  }
+})
+
